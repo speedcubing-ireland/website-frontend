fd7808d3bc0df74699430debf6c56ef6445ecddb
Simon Kelly <simonkellly@gmail.com>
2019-08-16 23:24:56 +0100

    Moved back to localhost

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // CONSTANTS ============================================================
 
-const API_URL = `https://speedcubing-backend.herokuapp.com`
+const API_URL = `http://localhost:1337`
 
 // Navbar ============================================================
 $(function(){
