df6e0f0c19e0352ca34cef0d830a5092ac1dff4f
Simon Kelly <simonkellly@gmail.com>
2019-09-24 21:01:30 +0100

    h2

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function createArticle({ title, author, date, contents, tagName }) {
   const header = document.createElement('div')
   header.className = 'message-header'
 
-  const titleElem = document.createElement('h1')
+  const titleElem = document.createElement('h2')
   titleElem.innerText = title
   titleElem.className = 'has-text-black'
 
